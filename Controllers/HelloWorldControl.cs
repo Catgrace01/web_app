f307c5ed25a7e12011a6bf6b9a98e81c453c0d86
Grace Hammond <catgrace01@gmail.com>
2021-04-29 17:55:49 -0500

    Fixed errors with GET:

--- Controllers/HelloWorldControl.cs
+++ Controllers/HelloWorldControl.cs
@@ -9,7 +9,7 @@ using web_app.Models;
 
 namespace web_app.Controllers{
     public class HelloWorldController : Controller{
-        //GET: /HelloWorld/
+        //GET: /HelloWorld/Index
         public string Index(){
             return Content("This is the default action.");
             return Content("Hello World UwU");
@@ -19,6 +19,7 @@ namespace web_app.Controllers{
             return Content("This is the Welcome action method.");
             return Content("Welcome!");
         }
+        //GET: /HelloWorld/Goodbye
         public string Goodbye(){
             return Content("This is the Goodbye action method.");
             return Content("Goodbye!");
