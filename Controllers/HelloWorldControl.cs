2e37400da0fdf3b9cfa09348f65a054c6cdd99d5
Grace Hammond <catgrace01@gmail.com>
2021-04-29 17:53:31 -0500

    Added Index, Welcome, and Goodbye action methods

--- Controllers/HelloWorldControl.cs
+++ Controllers/HelloWorldControl.cs
@@ -6,3 +6,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using web_app.Models;
+
+namespace web_app.Controllers{
+    public class HelloWorldController : Controller{
+        //GET: /HelloWorld/
+        public string Index(){
+            return Content("This is the default action.");
+            return Content("Hello World UwU");
+        }
+        //GET: /HelloWorld/Welcome/
+        public string Welcome(){
+            return Content("This is the Welcome action method.");
+            return Content("Welcome!");
+        }
+        public string Goodbye(){
+            return Content("This is the Goodbye action method.");
+            return Content("Goodbye!");
+        }
+    }
+}
